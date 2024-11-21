93153bf46bd57b6384ebad41b56840dd1fec579d
atre sachin <atresachin11@gmail.com>
2024-11-21 15:49:50 +0530

    removed all secretes from git

--- Backend/src/utils/firebaseAdmin.js
+++ Backend/src/utils/firebaseAdmin.js
@@ -1,9 +1,33 @@
-var admin = require("firebase-admin");
+// var admin = require("firebase-admin");
+//
+// var serviceAccount = require("./firebaseAdminSDK.json");
+//
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+//
+// module.exports = admin;
 
-var serviceAccount = require("./firebaseAdminSDK.json");
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: "smartdairy-flash-notifi",
+  private_key_id: process.env.FCMPRIVATE_KEY_ID,
+  private_key: process.env.FCMPRIVATE_KEY.replace(/\\n/g, "\n"), // Replace \n with actual newlines
+  client_email: process.env.FCMCLIENT_MAIL,
+  client_id: process.env.FCMCLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.CLIENT_CERT_URL,
+  universe_domain: "googleapis.com",
+};
+
+// Example: Initialize Firebase Admin SDK
+const admin = require("firebase-admin");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-module.exports = admin;
