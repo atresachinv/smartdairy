b7d5eee37a5057586c57f2086a8cfaf8fca8a932
Atre Pramod Namdev <pramodatre05@gmail.com>
2025-03-26 15:48:10 +0530

    credit pages and backend done

---
+++ Backend/Controllers/AcountController.js
@@ -0,0 +1,188 @@
+const pool = require("../Configs/Database");
+
+//get all Voucher
+exports.getAllVoucher = async (req, res) => {
+  const { dairy_id, center_id } = req.user;
+  const { autoCenter, ...filters } = req.query;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting MySQL connection: ", err);
+      return res
+        .status(500)
+        .json({ success: false, message: "Database connection error" });
+    }
+    // companyid; userid;VoucherNo; VoucherDate;// GLCode;// AccCode;// Amt;// Vtype;// InstrType;
+    // BillNo;// ReceiptType;// ChequeNo;// BankCode;// ChequeDate;// OpId;// OffId;// ScrollNo;// BatchNo;// ReceiptNo;// Narration;// IsInterest;// Narration1;// vibhag;// createdby;// createdon;
+    // updatedby;// updatedon;// pagetype;// cn;// ps;// isDeleted;// DeletedBy;// DeletedOn;// salebillno;
+    // salebilldate;// center_id;
+
+    let query = `SELECT * FROM tally_trnfile WHERE companyid = ? `;
+    let queryParams = [dairy_id];
+
+    if (autoCenter === 1) {
+      // If autoCenter is enabled, fetch only for the specific center
+      query += " AND center_id = ?";
+      queryParams.push(center_id);
+    } else if (center_id > 0) {
+      // If autoCenter is disabled, fetch items for both global (center_id = 0) and the specific center
+      query += " AND (center_id = 0 OR center_id = ?)";
+      queryParams.push(center_id);
+    }
+
+    if (filters) {
+      for (const [field, value] of Object.entries(filters)) {
+        if (value) {
+          query += ` AND ${field} = ?`;
+          queryParams.push(value);
+        }
+      }
+    }
+
+    connection.query(query, queryParams, (err, result) => {
+      connection.release();
+      if (err) {
+        console.error("Error executing query: ", err);
+        return res
+          .status(500)
+          .json({ success: false, message: "Database query error" });
+      }
+      if (result.length === 0) {
+        return res.status(201).json({
+          success: true,
+          message: "No found matching criteria!",
+          voucherData: [],
+        });
+      }
+      res.status(200).json({
+        success: true,
+        voucherData: result,
+      });
+    });
+  });
+};
+
+// Insert new voucher
+exports.insertNewVoucher = async (req, res) => {
+  const voucherData = req.body;
+  const { dairy_id, center_id } = req.user;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting MySQL connection: ", err);
+      return res
+        .status(500)
+        .json({ success: false, message: "Database connection error" });
+    }
+
+    // Dynamically create the insert query
+    const fields = Object.keys(voucherData).join(", ");
+    const placeholders = Object.keys(voucherData)
+      .map(() => "?")
+      .join(", ");
+    const values = Object.values(voucherData);
+
+    const query = `INSERT INTO tally_trnfile (companyid, center_id, ${fields}) VALUES (?, ?, ${placeholders})`;
+    const queryParams = [dairy_id, center_id, ...values];
+
+    connection.query(query, queryParams, (err, result) => {
+      connection.release();
+      if (err) {
+        console.error("Error executing query: ", err);
+        return res
+          .status(500)
+          .json({ success: false, message: "Database query error" });
+      }
+      res.status(200).json({
+        success: true,
+        message: "Voucher inserted successfully!",
+        result,
+      });
+    });
+  });
+};
+
+// Update voucher
+exports.updateVoucher = async (req, res) => {
+  const { id, ...updateData } = req.body;
+  const { dairy_id } = req.user;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting MySQL connection: ", err);
+      return res
+        .status(500)
+        .json({ success: false, message: "Database connection error" });
+    }
+
+    // Dynamically create the SET clause for the update query
+    const fields = Object.keys(updateData)
+      .map((field) => `${field} = ?`)
+      .join(", ");
+    const values = Object.values(updateData);
+
+    // Create the dynamic query
+    const query = `UPDATE tally_trnfile SET ${fields} WHERE id = ? AND companyid = ? `;
+    const queryParams = [...values, id, dairy_id];
+
+    connection.query(query, queryParams, (err, result) => {
+      connection.release();
+      if (err) {
+        console.error("Error executing query: ", err);
+        return res
+          .status(500)
+          .json({ success: false, message: "Database query error" });
+      }
+      if (result.affectedRows === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "No record found to update!",
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: "Voucher updated successfully!",
+        result,
+      });
+    });
+  });
+};
+
+// Delete voucher
+exports.deleteVoucher = async (req, res) => {
+  const { id } = req.query;
+  const { dairy_id } = req.user;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting MySQL connection: ", err);
+      return res
+        .status(500)
+        .json({ success: false, message: "Database connection error" });
+    }
+
+    // Create the delete query
+    const query = `DELETE FROM tally_trnfile WHERE id = ? AND companyid = ? `;
+    const queryParams = [id, dairy_id];
+
+    connection.query(query, queryParams, (err, result) => {
+      connection.release();
+      if (err) {
+        console.error("Error executing query: ", err);
+        return res
+          .status(500)
+          .json({ success: false, message: "Database query error" });
+      }
+      if (result.affectedRows === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "No record found to delete!",
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: "Voucher deleted successfully!",
+      });
+    });
+  });
+};
--- Frontend/src/Components/Mainapp/Accounts/Credit/Credit.jsx
+++ Frontend/src/Components/Mainapp/Accounts/Credit/Credit.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { MdDeleteOutline } from "react-icons/md";
 import Select from "react-select";
 import "./credit.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { listSubLedger } from "../../../../App/Features/Mainapp/Masters/ledgerSlice";
 import { listCustomer } from "../../../../App/Features/Customers/customerSlice";
 const Credit = () => {
@@ -11,7 +11,11 @@ const Credit = () => {
   const dispatch = useDispatch();
   const [formData, setFormData] = useState({
     srno: "",
+    DeductionId: "",
+    DeductionId2: "",
   });
+  const sledgerlist = useSelector((state) => state.ledger.sledgerlist);
+
   // Get master dates and list customer
   useEffect(() => {
     dispatch(listCustomer());
@@ -40,159 +44,284 @@ const Credit = () => {
     label: `${item.cname}`,
   }));
 
-  // handle cust Select Change---------->
-  const handleCustSelectChange = (selectedOption) => {
+  //option list show only name
+  const options = sledgerlist.map((i) => ({
+    value: i.lno,
+    label: i.marathi_name,
+  }));
+  //option list show only id
+  const options2 = sledgerlist.map((i) => ({
+    value: i.lno,
+    label: i.lno,
+  }));
+  // handle Select Change
+  const handleSelectChange = (selectedOption, keyToUpdate) => {
     setFormData({
       ...formData,
-      srno: selectedOption.value,
+      [keyToUpdate]: selectedOption.value,
     });
   };
 
   return (
     <div className="Credit-container w100 h1 d-flex-col">
-      <span className=" heading p10">व्यवहार भरणे</span>
-      <div className="credit-form d-flex-col mx10 px10 bg">
-        <div className="row d-flex w100 h20  sb a-center ">
-          <div className=" Deal-date-div d-flex a-center">
-            <span className="info-text w70">व्यवहार दिनांक</span>
-            <input type="date" className="data" />
-          </div>
-          <div className=" Batch No-div d-flex a-center">
-            <span className="info-text w40 ">Batch No:</span>
-            <input type="text" className="data h1" />
-          </div>
-        </div>
-        <div className="deal-container row w100 d-flex my10  sb a-center">
-          <div className=" Deal-typr-div  w50 h1 d-flex">
-            <span className="info-text w50">व्यवहार प्रकार</span>
-            <select className="data w40">
-              <option value=""> </option>
-              <option value="0">ट्रान्सफर </option>
-              <option value="1">रोख </option>
-            </select>
-          </div>
-          <div className=" bill-no-div w50 d-flex mx15 a-center">
-            <span className="info-text">चलन नंबर</span>
-            <input type="text" className="data w30" />
-          </div>
-        </div>
-        <div className="  bill-type-check-container row w100 d-flex my10 sb a-center">
-          <div className=" bill-type-div w50 d-flex a-center ">
-            <span className="info-text w50">पावती प्रकार</span>
-            <select className="data w40">
-              <option value=""> </option>
-              <option value="0">चेक </option>
-              <option value="1">कार्यालय चलन </option>
-            </select>
-          </div>
-          <div className=" bill-number-div w50 d-flex mx15 a-center">
-            <span className="info-text">पावती नं.</span>
-            <input type="text" className="data w30" />
-          </div>
-        </div>
-        <div className=" utpadak-number-container w100 h20
-            row sb d-flex my10"
-        >
-          <div className=" utpadak-number-div w70 d-flex a-center ">
-            <span className="info-text">उत्पादक क्रमांक</span>
-            <Select
-              options={custOptions}
-              className=" w20"
-              placeholder=""
-              isSearchable
-              styles={{
-                menu: (provided) => ({
-                  ...provided,
-                  zIndex: 200,
-                }),
-              }}
-              value={custOptions.find(
-                (option) => option.value === formData.srno
-              )}
-              onChange={handleCustSelectChange}
-            />
-            <Select
-              options={custOptions1}
-              className=" mx10 w80"
-              placeholder=""
-              isSearchable
-              styles={{
-                menu: (provided) => ({
-                  ...provided,
-                  zIndex: 200,
-                }),
-              }}
-              value={custOptions1.find(
-                (option) => option.value === formData.srno
-              )}
-              onChange={handleCustSelectChange}
-            />
-          </div>
-          <div className="Amount-button-container w30 h1  d-flex">
-            <div className=" Amountt-div d-flex a-center w60 ">
-              <span className="info-text">रक्कम</span>
-              <input type="text" className="data" />
+      <div className="Credit-container-scroll d-flex-col w100">
+        <span className=" heading p10">व्यवहार भरणे</span>
+        <div className="credit-all-form d-flex w100 ">
+          <div className="credit-form d-flex-col mx10 p10 bg">
+            <div className="row d-flex w100  sb a-center ">
+              <div className="w80  Deal-date-div d-flex a-center">
+                <span className="info-text  ">व्यवहार दिनांक</span>
+                <input type="date" className="data w60 " />
+              </div>
+              <div className="Batch No-div d-flex a-center mx10 sb">
+                <span className="info-text   ">Batch No:</span>
+                <input type="text" className="data w50" />
+              </div>
             </div>
-            <div className=" credit-add-delete-button w40 d-flex a-center">
-              <button className="w-btn mx10">Add</button>
-              <button className="w-btn">Delete</button>
+            <div className="deal-container row w100 d-flex my5  sb a-center">
+              <div className=" Deal-typr-div  w40  d-flex a-center">
+                <span className="info-text w50">व्यवहार प्रकार</span>
+                <select className="data w50">
+                  <option value=""> Select</option>
+                  <option value="0">ट्रान्सफर </option>
+                  <option value="1">रोख </option>
+                </select>
+              </div>
+              <div className=" bill-no-div w50 d-flex mx15 a-center sb">
+                <span className="info-text">चलन नंबर</span>
+                <input type="text" className="data w40" />
+              </div>
+            </div>
+            <div className="  bill-type-check-container row w100 d-flex my5 sb a-center">
+              <div className=" bill-type-div w50 d-flex a-center  ">
+                <span className="info-text w50">पावती प्रकार</span>
+                <select className="data w50">
+                  <option value="">select</option>
+                  <option value="0">चेक </option>
+                  <option value="1">कार्यालय चलन </option>
+                </select>
+              </div>
+              <div className=" bill-number-div w50 d-flex mx15 a-center sb">
+                <span className="info-text">पावती नं.</span>
+                <input type="text" className="data w40" />
+              </div>
+            </div>
+            <div
+              className="w100  
+            row sb d-flex my5"
+            >
+              <span className="info-text ">नावे खतावणी</span>
+              <Select
+                options={options2}
+                className=" w20"
+                placeholder=""
+                isSearchable
+                styles={{
+                  menu: (provided) => ({
+                    ...provided,
+                    zIndex: 200,
+                  }),
+                }}
+                value={options2.find(
+                  (option) => option.value === formData.DeductionId
+                )}
+                onChange={(selectedOption) =>
+                  handleSelectChange(selectedOption, "DeductionId")
+                }
+              />
+              <Select
+                options={options}
+                className=" mx10 w50"
+                placeholder=""
+                isSearchable
+                styles={{
+                  menu: (provided) => ({
+                    ...provided,
+                    zIndex: 200,
+                  }),
+                }}
+                value={options.find(
+                  (option) => option.value === formData.DeductionId
+                )}
+                onChange={(selectedOption) =>
+                  handleSelectChange(selectedOption, "DeductionId")
+                }
+              />
+            </div>
+            <div
+              className="  w100  
+            row sb d-flex my5"
+            >
+              <span className="info-text ">जमा खतावणी</span>
+              <Select
+                options={options2}
+                className=" w20"
+                placeholder=""
+                isSearchable
+                styles={{
+                  menu: (provided) => ({
+                    ...provided,
+                    zIndex: 200,
+                  }),
+                }}
+                value={options2.find(
+                  (option) => option.value === formData.DeductionId2
+                )}
+                onChange={(selectedOption) =>
+                  handleSelectChange(selectedOption, "DeductionId2")
+                }
+              />
+              <Select
+                options={options}
+                className=" mx10 w50"
+                placeholder=""
+                isSearchable
+                styles={{
+                  menu: (provided) => ({
+                    ...provided,
+                    zIndex: 200,
+                  }),
+                }}
+                value={options.find(
+                  (option) => option.value === formData.DeductionId2
+                )}
+                onChange={(selectedOption) =>
+                  handleSelectChange(selectedOption, "DeductionId2")
+                }
+              />
+            </div>
+            <div className=" w100  row sb d-flex my5">
+              <span className="info-text">खाते क्र.</span>
+              <Select
+                options={custOptions}
+                className=" w20"
+                placeholder=""
+                isSearchable
+                styles={{
+                  menu: (provided) => ({
+                    ...provided,
+                    zIndex: 200,
+                  }),
+                }}
+                value={custOptions.find(
+                  (option) => option.value === formData.srno
+                )}
+                onChange={(selectedOption) =>
+                  handleSelectChange(selectedOption, "srno")
+                }
+              />
+              <Select
+                options={custOptions1}
+                className=" mx10 w60"
+                placeholder=""
+                isSearchable
+                styles={{
+                  menu: (provided) => ({
+                    ...provided,
+                    zIndex: 200,
+                  }),
+                }}
+                value={custOptions1.find(
+                  (option) => option.value === formData.srno
+                )}
+                onChange={(selectedOption) =>
+                  handleSelectChange(selectedOption, "srno")
+                }
+              />
+            </div>
+            <div className="Amount-button-container  d-flex sb">
+              <div className=" Amountt-div d-flex a-center w40 ">
+                <span className="info-text ">रक्कम</span>
+                <input type="text" className="data" />
+              </div>
+              <div className=" Amountt-div d-flex a-center w40 ">
+                <span className="info-text">आज बॅलेन्स </span>
+                <input type="text" className="data" />
+              </div>
+            </div>
+            <div className="Amount-button-container  w100 d-flex sb">
+              <div className=" Amountt-div d-flex a-center  w60 mx5">
+                <span className="info-text ">तपशील </span>
+                <input type="text" className="data  " />
+              </div>
+              <div className=" Amountt-div d-flex a-center  ">
+                <input type="checkbox" name="" id="" />
+                <span className="info-text mx10 w100 ">एक खतावणी व्यवहार</span>
+              </div>
             </div>
           </div>
+          <div className="credit-batchTally d-flex-col mx5  bg ">
+            <div className="m10 p10 br">
+              बॅच टॅलि
+              <div className="d-flex a-center ">
+                <span className="w50 info-text">नावे </span>
+                <input type="text" className="data" />
+              </div>
+              <div className="d-flex a-center my10">
+                <span className="w50 info-text">जमा </span>
+                <input type="text" className="data" />
+              </div>
+              <div className="d-flex a-center ">
+                <span className="w50 info-text">बॅच फरक </span>
+                <span className="w50 req info-text">{0} </span>
+              </div>
+            </div>
+            <div className="m10 p10 br">
+              चेक/डी.डी. माहिती
+              <div className="d-flex a-center ">
+                <span className="w50 info-text"> चेक/डी.डी.</span>
+                <input type="text" className="data" />
+              </div>
+              <div className="d-flex a-center my10  ">
+                <span className="  info-text">दिनांक </span>
+                <input type="date" className="w70 data " />
+              </div>
+            </div>
+          </div>
+          <div className="credit-batchTally-buttons d-flex-col mx10 p10 bg">
+            <button className="w-btn ">नवीन बॅच</button>
+            <button className="w-btn ">नवीन चलन</button>
+            <button className="w-btn ">सेव करा</button>
+            <button className="w-btn ">अपडेट करा</button>
+          </div>
         </div>
-      </div>
-      <div className="credit-table d-flex-col m10 px10 h1 bg">
-        <div className="table-container">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Edit</th>
-                <th>चलन</th>
-                <th>ख. नं.</th>
-                <th>खतावणी नाव</th>
-                <th>खाते</th>
-                <th>खातेदार नाव</th>
-                <th>रक्कम</th>
-                <th>व्यवहार</th>
-                <th>बॅच</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <FaRegEdit className="icon" />
-                </td>
-                <td>चलन</td>
-                <td>ख. नं.</td>
-                <td>खतावणी नाव</td>
-                <td>खाते</td>
-                <td>खातेदार नाव</td>
-                <td>रक्कम</td>
-                <td>व्यवहार</td>
-                <td>बॅच</td>
-                <td>
-                  {" "}
-                  <MdDeleteOutline className="icon" />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <FaRegEdit className="icon" />
-                </td>
-                <td>चलन</td>
-                <td>ख. नं.</td>
-                <td>खतावणी नाव</td>
-                <td>खाते</td>
-                <td>खातेदार नाव</td>
-                <td>रक्कम</td>
-                <td>व्यवहार</td>
-                <td>बॅच</td>
-                <td>
-                  <MdDeleteOutline className="icon" />
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="credit-table d-flex-col m10 px10 h1 bg">
+          <div className="table-container">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Edit</th>
+                  <th>चलन</th>
+                  <th>ख. नं.</th>
+                  <th>खतावणी नाव</th>
+                  <th>खाते</th>
+                  <th>खातेदार नाव</th>
+                  <th>रक्कम</th>
+                  <th>व्यवहार</th>
+                  <th>बॅच</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <FaRegEdit className="icon" />
+                  </td>
+                  <td className="info-text">चलन</td>
+                  <td className="info-text">ख. नं.</td>
+                  <td className="info-text">खतावणी नाव</td>
+                  <td className="info-text">खाते</td>
+                  <td className="info-text">खातेदार नाव</td>
+                  <td className="info-text">रक्कम</td>
+                  <td className="info-text">व्यवहार</td>
+                  <td className="info-text">बॅच</td>
+                  <td>
+                    <MdDeleteOutline className="icon" />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
