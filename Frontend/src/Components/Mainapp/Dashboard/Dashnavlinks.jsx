ab8aca257cdfae7eab8c6af14b0479e72d6351d0
Atre Pramod Namdev <pramodatre05@gmail.com>
2025-04-07 15:03:05 +0530

    update dashboard nav toadd invertory-dashboard

---
+++ Frontend/src/Components/Mainapp/Dashboard/Dashnavlinks.jsx
@@ -0,0 +1,45 @@
+import { useTranslation } from "react-i18next";
+import { BsHouseFill } from "react-icons/bs";
+import { TbMilk } from "react-icons/tb";
+import { NavLink } from "react-router-dom";
+
+const Dashnavlinks = ({ isselected, setIsSelected }) => {
+  const { t } = useTranslation(["puchasesale", "common"]);
+  const Navbuttons = [
+    {
+      name: `${t("मिल्क डॅशबोर्ड")}`,
+      icon: <TbMilk className="icon" />,
+      path: "milk-dashboard",
+    },
+    {
+      name: `${t("इन्व्हेंटरी डॅशबोर्ड")}`,
+      icon: <BsHouseFill className="icon" />,
+      path: "invertory-dashboard",
+    },
+  ];
+  return (
+    <>
+      {Navbuttons.map((button, index) => (
+        <li
+          key={index}
+          className={`home-nav-item  d-flex a-center ${
+            isselected === index ? "selected" : ""
+          }`}
+        >
+          <NavLink
+            to={button.path}
+            onClick={() => {
+              setIsSelected(index);
+            }}
+            className="sub-navlinks f-label-text"
+          >
+            {button.icon}
+            <span>{button.name}</span>
+          </NavLink>
+        </li>
+      ))}
+    </>
+  );
+};
+
+export default Dashnavlinks;
