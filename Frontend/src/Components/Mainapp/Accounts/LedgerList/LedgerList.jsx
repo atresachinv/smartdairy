f13d907f674162440aedd383cb0cb408c01c5807
Atre Pramod Namdev <pramodatre05@gmail.com>
2025-04-17 10:33:49 +0530

    Add getGroupedVoucher endpoint and update routes for voucher grouping

--- Frontend/src/Components/Mainapp/Accounts/LedgerList/LedgerList.jsx
+++ Frontend/src/Components/Mainapp/Accounts/LedgerList/LedgerList.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { listSubLedger } from "../../../../App/Features/Mainapp/Masters/ledgerSlice";
 import Select from "react-select";
 import { toast } from "react-toastify";
 import axiosInstance from "../../../../App/axiosInstance";
+import { listCustomer } from "../../../../App/Features/Customers/customerSlice";
 
 //gett todays date------------>
 const getTodaysDate = () => {
@@ -14,12 +15,31 @@ const getTodaysDate = () => {
 
 const LedgerList = () => {
   const dispatch = useDispatch();
+  const [customerList, setCustomerList] = useState([]);
   const sledgerlist = useSelector((state) => state.ledger.sledgerlist);
   const [formData, setFormData] = useState({
     GLCode: "",
     date: getTodaysDate(),
   });
   const [voucherList, setVoucherlist] = useState([]);
+  const [filterVoucherList, setFilterVoucherlist] = useState([]);
+  const [filter, setFilter] = useState(0);
+  const centerSetting = useSelector(
+    (state) => state.dairySetting.centerSetting
+  );
+  const centerList = useSelector(
+    (state) => state.center.centersList.centersDetails || []
+  );
+  const [settings, setSettings] = useState({});
+  const autoCenter = useMemo(() => settings?.autoCenter, [settings]);
+  const centerId = useSelector((state) => state.dairy.dairyData.center_id);
+  const [loading, setLoading] = useState(false);
+  //set setting
+  useEffect(() => {
+    if (centerSetting?.length > 0) {
+      setSettings(centerSetting[0]);
+    }
+  }, [centerSetting]);
 
   //option list show only name
   const options = sledgerlist.map((i) => ({
@@ -29,7 +49,36 @@ const LedgerList = () => {
 
   useEffect(() => {
     dispatch(listSubLedger());
+    dispatch(listCustomer());
   }, []);
+
+  //----------------------------------------------------------------->
+  // Effect to load customer list from local storage
+  useEffect(() => {
+    const storedCustomerList = localStorage.getItem("customerlist");
+    if (storedCustomerList) {
+      if (centerId === 0) {
+        const filteredCustomerList = JSON.parse(storedCustomerList).filter(
+          (customer) => Number(customer.centerid) === Number(filter)
+        );
+        setCustomerList(filteredCustomerList);
+
+        const filteredVoucherList = voucherList.filter(
+          (item) => Number(item.center_id) === Number(filter)
+        );
+        setFilterVoucherlist(filteredVoucherList);
+      } else {
+        const filteredCustomerList = JSON.parse(storedCustomerList).filter(
+          (customer) => Number(customer.centerid) === Number(centerId)
+        );
+        setCustomerList(filteredCustomerList);
+        const filteredVoucherList = voucherList.filter(
+          (item) => Number(item.center_id) === Number(centerId)
+        );
+        setFilterVoucherlist(filteredVoucherList);
+      }
+    }
+  }, [centerId, filter, voucherList]);
 
   // handle Select Change
   const handleSelectChange = (selectedOption, keyToUpdate) => {
@@ -42,16 +91,20 @@ const LedgerList = () => {
   const handleFetchData = async (e) => {
     e.preventDefault();
     if (formData.GLCode && formData.date) {
+      setLoading(true);
       try {
+        //   GLCode, autoCenter, VoucherDate;
         const res = await axiosInstance.get(
-          `/voucher-sublegder-list?glcode=${formData.GLCode}&date${formData.date}`
+          `/voucher-sublegder-list?GLCode=${formData.GLCode}&VoucherDate=${formData.date}&autoCenter=${autoCenter}`
         );
         if (res.status === 200) {
           setVoucherlist(res.data?.voucherList || []);
         }
+        setLoading(false);
       } catch (error) {
         // console.error("Error fetching data:", error);
         toast.error("Server falied fetching data");
+        setLoading(false);
       }
     } else {
       toast.error("Please fill all the fields");
@@ -61,6 +114,28 @@ const LedgerList = () => {
   return (
     <div className="w100 h1 d-flex-col m10 ">
       <div className="w100 p10 bg">
+        {centerId > 0 ? (
+          <></>
+        ) : (
+          <div className="d-flex a-center mx10">
+            <span className="info-text">सेंटर निवडा :</span>
+            <select
+              className="data w50 a-center  my5 mx5"
+              name="center"
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+            >
+              {centerList &&
+                [...centerList]
+                  .sort((a, b) => a.center_id - b.center_id)
+                  .map((center, index) => (
+                    <option key={index} value={center.center_id}>
+                      {center.center_name}
+                    </option>
+                  ))}
+            </select>
+          </div>
+        )}
         <div className="w100 d-flex center sa f-wrap">
           <input
             type="date"
@@ -130,13 +205,23 @@ const LedgerList = () => {
         <div style={{ maxHeight: "456px", overflowY: "auto" }}>
           <table className="account-legder-table w100">
             <tbody>
-              {voucherList.length > 0 ? (
-                voucherList.map((item, i) => (
+              {loading ? (
+                <tr>
+                  <td colSpan="4" style={{ textAlign: "center" }}>
+                    Loading...
+                  </td>
+                </tr>
+              ) : filterVoucherList.length > 0 ? (
+                filterVoucherList.map((item, i) => (
                   <tr key={i}>
-                    <td>{item.Accode}</td>
-                    <td>{item.AccName}</td>
-                    <td>{item.Amount}</td>
-                    <td>{item.v_type}</td>
+                    <td>{item.AccCode}</td>
+                    <td>
+                      {customerList.find((i) => i.srno === item.AccCode)
+                        ?.engName || "-"}
+                    </td>
+
+                    <td>{item.totalamt}</td>
+                    <td>{item.totalamt < 0 ? "नावे" : "जमा"}</td>
                   </tr>
                 ))
               ) : (
