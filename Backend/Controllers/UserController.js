a730d122dd3ba3d0b4103a6dacc184a833c393a0
atresachinv <atresachin11@gamil.com>
2025-07-09 17:33:47 +0530

    origin error fix2

--- Backend/Controllers/UserController.js
+++ Backend/Controllers/UserController.js
@@ -6,7 +6,6 @@ const nodemailer = require("nodemailer");
 const crypto = require("crypto");
 dotenv.config({ path: "Backend/.env" });
 
-
 const getConnectionPromise = () => {
   return new Promise((resolve, reject) => {
     pool.getConnection((err, connection) => {
@@ -480,7 +479,7 @@ exports.setupBasicInformation = async (req, res) => {
     await queryPromise(
       connection,
       `INSERT INTO bankmaster (code, name, branch, ifsc, companyid, center_id) VALUES (?, ?, ?, ?, ?, ?)`,
-      [1,"Demo Bank", "Demo", "DEMO000123",SocietyCode, 0]
+      [1, "Demo Bank", "Demo", "DEMO000123", SocietyCode, 0]
     );
 
     await queryPromise(
@@ -512,7 +511,6 @@ exports.setupBasicInformation = async (req, res) => {
     });
   }
 };
-
 
 //---------------------------------------------------------------------------->
 // Login --------------------------------------------------------------------->
@@ -623,7 +621,7 @@ exports.userLogin = async (req, res) => {
             res.cookie("token", token, {
               httpOnly: true,
               secure: true,
-              sameSite: "none",
+              sameSite: "None",
               maxAge: 4 * 60 * 60 * 1000,
             });
 
